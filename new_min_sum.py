a8c2cfb9d06c95e0f35cf0f4d2de6f677b37f85b
Alexios Mantas <alexiosmantas@Alexioss-MacBook-Air.local>
2019-09-03 21:43:55 +0300

    LQnm not defined. H created.

--- new_min_sum.py
+++ new_min_sum.py
@@ -7,53 +7,73 @@ H_positions = []
 
 num_of_lines = int(os.popen('cat H.txt | wc -l').read())
 print num_of_lines
-# H.txt has size of 384x32 => 32 CN and 384 BN
 
+# find max element number in H.txt which will be the one distance of the H
+max_val = 0
 for line in open('H.txt','r').readlines():
-	H_positions.append(len(line.split(',')[:-1] ))
-# check that all elements in H_positions are equal
-if all(elem == H_positions[0] for elem in H_positions):
-	print 'Tanner graph is uniform'
-else:
-	print 'Check Nodes check different numbers of Binary Nodes'
+	print line.split(',')[:-1]
+	tmp_max = max(map(int,(line.split(',')[:-1])))
+	if  tmp_max > max_val :
+		print str(tmp_max)+' greater than '+str(max_val)
+		max_val = tmp_max
+	print max_val
 
-H = np.zeros((H_positions[0],num_of_lines))
-for line in open('H.txt','r').readlines():
-	for elem in line.split(',')[:-1]:
-		H[elem,line] = 1
+print max_val
+print num_of_lines
+
+M = num_of_lines
+N = max_val
+
+H = np.zeros((M,N))
+for line,line_num in zip(open('H.txt','r').readlines(),range(M)):
+	for elem in map(int,line.split(',')[:-1]):
+		H[line_num-1,elem-1] = 1
+
+print H
+H.shape
+
+ran = 300
+print 'Each CN is connected to '+ str(np.sum(H[ran,:])) + ' BNs'
+print 'Each BN is connected to '+ str(np.sum(H[:,ran])) + ' CNs'
 
 
-'''
+
 # LDPC Min Sum
 MAX_ITERATIONS = 100
-
+variance = 1
 # construction of the incoming signal vector
-M = np.shape(H)[0]
-N = np.shape(H)[1]
+#M = np.shape(H)[0]
+#N = np.shape(H)[1]
 awgn = np.random.normal(0,variance,N)
 signal = np.random.choice([-1,1],N) # random vector of +1 -1
 
 y = signal + awgn
 print y
 
-variance = 1
+fake_encoded_y = np.zeros(N)
+fake_encoded_y[1] = 1
+print fake_encoded_y
+y=fake_encoded_y
+
+
+
 
 LPn0 = 2*y/variance # vector 1xN
-
+iteration = 0
 while (iteration < MAX_ITERATIONS):
 	# update message to be send from each CN to each BN
 	for n in range(N):
 		for m in range(M):
 			H_row = H[m,:] # take all BNs connected to the specific CN
-			H_row[m,n] = 0 # except the specific BN for which the message is to be sent
-			LRmn[m,n] = np.prod(np.sign(tmp[np.multiply(H_row,LQnm) != 0])) * np.amin(np.abs(tmp[np.multiply(H_row,LQnm) != 0]))
+			H_row[n] = 0 # except the specific BN for which the message is to be sent
+			LRmn[m,n] = np.prod(np.sign([np.multiply(H_row,LQnm) != 0])) * np.amin(np.abs([np.multiply(H_row,LQnm) != 0]))
 
 	for n in range(N): # adding vectors, element by element, aims to update the state of each BN
 		H_col = H[:,n] # LP, which is the new state of a specific BN is the initial (prior probability of that BN) and the messages received from all the attached CNs to it
 		LP[n] = LPn0[n] +  np.sum(np.multiply(LRnm[:,n],H_col)) 
 
 	# update messages from each BN to each CN attached to it
-	if iteration = 0:
+	if iteration == 0:
 		tmp_val = np.repeat(LPn0,M).reshape(N,M).T # columns fulfilled with the initial (a prior) BN probabilities  
 		LQ = np.multiply(tmp_val,H) # Hadamard product with H, so that we get LQ with messages only in the attach CN of each BN 
 	else:
@@ -71,10 +91,8 @@ while (iteration < MAX_ITERATIONS):
 	if False not in  residue == 0:
 		print "result is:"
 		print C
-		return
+	        exit(0)	
 	else:
 		iteration += 1
 		continue
 print "Decoding failed" 
-'''
-
